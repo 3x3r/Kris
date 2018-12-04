e758a380c33ec809ac318be038f2c6b071f47a82
3x3r <45569507+3x3r@users.noreply.github.com>
2018-12-04 20:14:35 +0300

    Add files via upload

--- Kris.py
+++ Kris.py
@@ -4,7 +4,7 @@ import datetime
 import time
 violations={'tSoliddLine':'/report/targetinfo/tSolidLine/text()','nRedLight':'/report/targetinfo/nRedLight/text()','nWrongDirection':'/report/targetinfo/nWrongDirection/text()','tRoadSide':'/report/targetinfo/tRoadSide/text()','tStopLine':'/report/targetinfo/tStopLine/text()','tOneDirection':'/report/targetinfo/tOneDirection/text()','nOverSpeed':'/report/targetinfo/nOverSpeed/text()'}
 forsage={'F401':['F401'],'F402':['F402'],'F403':['F403'],'F404':['F404'],'F405':['F405'],'F406':['F406'],'F407':['F407'],'F408':['F408'],'F409':['F409'],'F410':['F410'],'F413':['F413']}
-
+violations_name={'tSoliddLine':'12_16_1','nRedLight':'12_15_1','nWrongDirection':'12_15_4','tRoadSide':'12_15_1','tStopLine':'12_12_2','tOneDirection':'TEST','nOverSpeed':'12_19'}
 forsage100={'F401':{'tSolidLine':0,'nRedLight':0,'nWrongDirection':0,'tRoadSide':0,'tStopLine':0,'tOneDirection':0,'nOverSpeed':0},
 'F402':{'tSolidLine':0,'nRedLight':0,'nWrongDirection':0,'tRoadSide':0,'tStopLine':0,'tOneDirection':0,'nOverSpeed':0},
 'F403': {'tSolidLine': 0, 'nRedLight': 0, 'nWrongDirection': 0, 'tRoadSide': 0, 'tStopLine': 0,'tOneDirection': 0, 'nOverSpeed': 0},
@@ -65,7 +65,7 @@ end_d=28
 end_m=11
 end_y=2018
 end_hh=9
-end_mm=40
+end_mm=25
 end_ss=00
 print(end_d,' ',end_m,' ',end_y,' ',end_hh,' ',end_mm,' ',end_ss)
 starttime=int(timeunix(start_d,start_m,start_y,start_hh,start_mm,start_ss))
@@ -91,13 +91,21 @@ for rootdir, dirs, files in os.walk(path): #парсинг пути
                      #   solid = solid+1
                       #  print ('test',solid)
 # print(tree.xpath('/report/targetinfo/tSolidLine/text()'))
-print(list_in)
+###print(list_in)
 #ss=listToStringWithoutBrackets(list_in) #создаем из списка строкуи удаляем квадратные скобки[ ]
 #print (ss)
 #list_out = ss.split(',') # из строки создаем список
-#print(list_out)
+#print(list_out),
 #print(list_out[2])
-for i in list_in:
-    for n,b in forsage.items():
-            if i==n:
-                print('TEST',i)
+#for i in range(len(list_in)):
+#    for n,b in forsage100.items():
+#        if list_in[i]==n:
+#            for name_viol in violations_name.items():
+#                    if name_viol == list_in[i+1]:
+#                        print('TEST',list_in[i],'Violation',list_in[i+1])
+i=10
+forsage100['F401']['tSolidLine']+=3
+print('Test1',forsage100.keys())#(['F401', 'F402', 'F403', 'F404', 'F405', 'F406', 'F407', 'F408', 'F409', 'F413'])
+print('Test3',forsage100['F401'].values()) #([3, 0, 0, 0, 0, 0, 0])
+print('Test4',forsage100['F401'].keys()) #(['tSolidLine', 'nRedLight', 'nWrongDirection', 'tRoadSide', 'tStopLine', 'tOneDirection', 'nOverSpeed'])
+print('TEST',forsage100['F401']['tSolidLine'])
